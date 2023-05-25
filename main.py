850b4322e85eb55a424ec7b67f05c2439dfd4417
Amish ali <63059501+Amish05@users.noreply.github.com>
2023-05-25 22:08:24 +0500

    main.py

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+import json
 
-app = FastAPI()
+# Listen for commands and respond accordingly
+while True:
+    command = input()  # Wait for input command
+    response = {}
 
-class Msg(BaseModel):
-    msg: str
+    if command == 'hello':
+        response['message'] = 'Hello from Python script!'
+    elif command == 'bye':
+        response['message'] = 'Goodbye from Python script!'
+    else:
+        response['message'] = 'Unknown command'
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World. Welcome to FastAPI!"}
-
-
-@app.get("/path")
-async def demo_get():
-    return {"message": "This is /path endpoint, use a post request to transform the text to uppercase"}
-
-
-@app.post("/path")
-async def demo_post(inp: Msg):
-    return {"message": inp.msg.upper()}
-
-
-@app.get("/path/{path_id}")
-async def demo_get_path_id(path_id: int):
-    return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+    # Send the response as JSON
+    print(json.dumps(response))
